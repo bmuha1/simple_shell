9db1ce1f4c012d37aa6b9bfc0b16cfbe6cdfafbf
rajsudeep <sudrocks@gmail.com>
2019-08-20 20:33:17 +0000

    Add signatures for builtin ft

--- shell.h
+++ shell.h
@@ -14,6 +14,17 @@ int _strlen(const char *s);
 char *_strstr(char *haystack, const char *needle);
 char *_strchr(char *s, char c);
 
-
+/* builtin functions and handlers */
+typedef struct cmd_s
+{
+	char *type;
+	int (*exec_built_in)(char **args);
+} cmd_t;
+int (*get_built_in(char *cmd))(char **args);
+int simple_exit(char **args);
+int simple_env(char **args);
+int simple_unsetenv(char **args);
+int simple_setenv(char **args);
+int not_built_in(char **args);
 
 #endif /* SHELL_H */
