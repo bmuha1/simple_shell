948366fa5037084cd05c51e6fa975aa4998c4156
Brent Muha <800@holbertonschool.com>
2019-08-22 16:50:12 +0000

    Remove comments

---
+++ remove_comments.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * remove_comments - Remove comments from the line of user input
+ * @line: The user input
+ */
+void remove_comments(char *line)
+{
+	int i;
+
+	for (i = 0; line[i] != '\0'; i++)
+	{
+		if (line[i] == '#')
+		{
+			line[i] = '\0';
+			break;
+		}
+	}
+}
