70c9aa08e62870815cef7e9fcdd111c9db26f44b
Brent Muha <800@holbertonschool.com>
2019-08-22 22:05:34 +0000

    Add $ variable replacement function

--- remove_comments.c
+++ remove_comments.c
@@ -17,3 +17,44 @@ void remove_comments(char *line)
 		}
 	}
 }
+
+/**
+ * replace_dollars - Replace $$, $?, and $ variable replacement
+ * @args: The arguments
+ * @env: The environment
+ */
+void replace_dollars(char **args, char **env)
+{
+	int i, j, pid;
+	char pid_string[12];
+
+	for (i = 0; i < 12; i++)
+		pid_string[i] = '\0';
+
+	(void) env;
+
+	for (i = 0; args[i] != NULL; i++)
+	{
+		if (args[i][0] == '$')
+		{
+			if (args[i][1] == '$')
+			{
+				free(args[i]);
+				pid = getpid();
+				_ntoa_rev(pid, pid_string);
+				rev_string(pid_string);
+				args[i] = _strdup(pid_string);
+			}
+			else if (args[i][1] == '?')
+			{
+				/* handle $? */
+				for (j = 0; env[j] != NULL; j++)
+					printf("env[%d]: %s\n", j, env[j]);
+			}
+			else
+			{
+				/* handle variable replacement */
+			}
+		}
+	}
+}
