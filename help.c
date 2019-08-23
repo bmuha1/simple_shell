238941ad94edef6285b45a727eff08bf7f562148
Brent Muha <800@holbertonschool.com>
2019-08-23 19:40:11 +0000

    Create individual help messages

--- help.c
+++ help.c
@@ -11,19 +11,19 @@ void which_help(char *command)
 	char unsetenv[] = "unsetenv help\n";
 	char no_match[] = "no match found\n";
 
-	if (_strcmp(command, "alias") == 0)
+	if (_strcmp("alias", command) == 0)
 		write(STDOUT_FILENO, alias, _strlen(alias));
-	else if (_strcmp(command, "cd") == 0)
+	else if (_strcmp("cd", command) == 0)
 		write(STDOUT_FILENO, cd, _strlen(cd));
-	else if (_strcmp(command, "env") == 0)
+	else if (_strcmp("env", command) == 0)
 		write(STDOUT_FILENO, env, _strlen(env));
-	else if (_strcmp(command, "exit") == 0)
+	else if (_strcmp("exit", command) == 0)
 		write(STDOUT_FILENO, exit, _strlen(exit));
-	else if (_strcmp(command, "help") == 0)
+	else if (_strcmp("help", command) == 0)
 		write(STDOUT_FILENO, help, _strlen(help));
-	else if (_strcmp(command, "setenv") == 0)
+	else if (_strcmp("setenv", command) == 0)
 		write(STDOUT_FILENO, setenv, _strlen(setenv));
-	else if (_strcmp(command, "unsetenv") == 0)
+	else if (_strcmp("unsetenv", command) == 0)
 		write(STDOUT_FILENO, unsetenv, _strlen(unsetenv));
 	else
 		write(STDOUT_FILENO, no_match, _strlen(no_match));
