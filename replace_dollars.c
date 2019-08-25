fbb176b10692416ea9e6b54879b201006a0b03fb
Brent Muha <800@holbertonschool.com>
2019-08-25 22:06:33 +0000

    Refactor to work with linked list environment

--- replace_dollars.c
+++ replace_dollars.c
@@ -8,14 +8,14 @@
 void replace_dollars(char **args, list_t *env)
 {
 	int i, pid;
-	char pid_string[12];
-	char *tmp = NULL, *env_var = NULL, *equal_location = NULL;
+	char pid_string[12] = "";
+	char *tmp = NULL;
 
 	for (i = 0; i < 12; i++)
 		pid_string[i] = '\0';
 	for (i = 0; args[i] != NULL; i++)
 	{
-		if (args[i][0] == '$')
+		if (args[i][0] == '$' && args[i][1] != '\0')
 		{
 			if (args[i][1] == '$')
 			{
@@ -25,21 +25,18 @@ void replace_dollars(char **args, list_t *env)
 				rev_string(pid_string);
 				args[i] = _strdup(pid_string);
 			}
+			else if (args[i][1] == '?')
+			{
+				free(args[i]);
+				tmp = _getenv_value("old_status", env);
+				args[i] = _strdup(tmp);
+			}
 			else
 			{
-				env_var = &args[i][1];
-				tmp = _getenv(env_var, env);
-				if (tmp)
-				{
-					equal_location = _strchr(tmp, '=');
-					free(args[i]);
-					args[i] = _strdup(equal_location + 1);
-				}
-				else
-				{
-					free(args[i]);
-					args[i] = _strdup("");
-				}
+				tmp = _strdup(&args[i][1]);
+				free(args[i]);
+				args[i] = _strdup(_getenv_value(tmp, env));
+				free(tmp);
 			}
 		}
 	}
