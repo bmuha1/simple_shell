7b11b1f1f432a42f6b107e42ab8d92bcf53fd470
Brent Muha <800@holbertonschool.com>
2019-08-25 22:06:03 +0000

    Add proper error handling

--- execute.c
+++ execute.c
@@ -3,23 +3,31 @@
 /**
  * execute - creates a new process to execute a command
  * @args: NULL terminated array of argument strings
+ * @env: The environmental variables
  *
  * Return: On success of execution 0, 1 on failure
  */
-int execute(char **args)
+int execute(char **args, list_t *env)
 {
 	pid_t child;
 	int i, status = 0;
 
 	child = fork();
 	if (child == -1)
-		exit(98);
+		return (98);
 	else if (child == 0)
 	{
 		if (execve(args[0], args, NULL) == -1)
 		{
-			perror(args[0]);
-			exit(EXIT_SUCCESS);
+			write(STDERR_FILENO, _getenv_value("argv", env),
+			      _strlen(_getenv_value("argv", env)));
+			write(STDERR_FILENO, ": ", 2);
+			write(STDERR_FILENO, _getenv_value("count", env),
+			      _strlen(_getenv_value("count", env)));
+			write(STDERR_FILENO, ": ", 2);
+			write(STDERR_FILENO, args[0], _strlen(args[0]));
+			write(STDERR_FILENO, ": not found\n", 12);
+			return (127);
 		}
 	}
 	else
