c866e1a936c96d0622c9cf9cac391813aade1067
Brent Muha <800@holbertonschool.com>
2019-08-25 22:31:00 +0000

    Fix multiple exit issue

--- execute.c
+++ execute.c
@@ -27,7 +27,7 @@ int execute(char **args, list_t *env)
 			write(STDERR_FILENO, ": ", 2);
 			write(STDERR_FILENO, args[0], _strlen(args[0]));
 			write(STDERR_FILENO, ": not found\n", 12);
-			return (127);
+			exit(127);
 		}
 	}
 	else
