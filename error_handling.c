e34b880e8096a19308170249ef697ea51b71bfd3
Brent Muha <800@holbertonschool.com>
2019-08-26 17:32:21 +0000

    Separate error handling into its own file

---
+++ error_handling.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * print_error - Print error message
+ * @args: NULL terminated array of argument strings
+ * @env: The environmental variables
+ * @message: The error message
+ */
+void print_error(char **args, list_t *env, char *message)
+{
+	write(STDERR_FILENO, _getenv_value("argv", env),
+	      _strlen(_getenv_value("argv", env)));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, _getenv_value("count", env),
+	      _strlen(_getenv_value("count", env)));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, args[0], _strlen(args[0]));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, message, _strlen(message));
+	write(STDERR_FILENO, "\n", 1);
+}
