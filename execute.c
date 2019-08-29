e6b87df20172b022b2aacfc038f19422ce875bc5
rajsudeep <sudrocks@gmail.com>
2019-08-29 05:08:36 +0000

    Remove error message if child pid returns -1

--- execute.c
+++ execute.c
@@ -16,7 +16,6 @@ int execute(char **args, list_t *env, status_t *status)
 	child = fork();
 	if (child == -1)
 	{
-		print_error(args, env, "Fork failed", status);
 		return (1);
 	}
 	else if (child == 0)
